2ac5147c69866c8206fa632e016cc3c116e41d81
janaina-passos <janaina.diogopassos@gmail.com>
2025-11-01 09:40:10 -0300

    Update Espetaculo.java

--- Projeto/Espetaculo.java
+++ Projeto/Espetaculo.java
@@ -6,5 +6,24 @@ public class Espetaculo {
     private double preco;
     private boolean assentos;
 
+    public void apresentaAssentos(){
+        
+    }
+    
+    //public Entrada novaEntrada(int tipo, int assento){
+
+    public double getPreco(){
+
+    }
+
+    public String toString(){
+
+    }
+
+    private void marcarAssento(int assento){
+
+    }
+    
+
 
 }
