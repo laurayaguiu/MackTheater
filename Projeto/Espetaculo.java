9344d78b12c78b97f7ad6ccdc538325b5f894803
janaina-passos <janaina.diogopassos@gmail.com>
2025-11-01 10:12:57 -0300

    Update Espetaculo.java

--- Projeto/Espetaculo.java
+++ Projeto/Espetaculo.java
@@ -3,20 +3,34 @@ public class Espetaculo {
     
     private String nome;
     private String data;
+    private String hora;
     private double preco;
-    private boolean assentos;
+    private boolean [50] assentos; 
+
+    //construtor
+    public Espetaculo(String nome, String data, String hora, double preco){
+        this.nome = nome;
+        this.data = data;
+        this.hora = hora;
+        this.preco = preco;
+    }
+
+    public double getPreco(){
+        return preco;
+    }
+
+    public String toString(){
+        return "Espetáculo: " + nome + "\n" + 
+            "Data: " + data + "\n" +
+            "Hora:" + hora + "\n" + 
+            "Preço da entrada inteira: R$ " + preco; 
+    }
 
     public void apresentaAssentos(){
         
     }
     
-    //public Entrada novaEntrada(int tipo, int assento){
-
-    public double getPreco(){
-
-    }
-
-    public String toString(){
+    public Entrada novaEntrada(int tipo, int assento){
 
     }
 
