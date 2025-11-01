ff9e675c6f29bb77b9ccf2bdcdf22926eca84b45
Laura Yaguiu <laurayaguiu@gmail.com>
2025-11-01 22:38:32 +0000

    .

--- Projeto/Main.java
+++ Projeto/Main.java
@@ -1,4 +1,9 @@
-//Janaína Diogo Passos - RA: 10737888, Bárbara Diogo Passos - RA: 10737885, Laura 
+/*
+Laura Rocha Yaguiu RA:10736399
+Janaína Passos RA: 10737888
+Bárbara Passos RA: 10737885
+*/
+
 import java.util.Scanner;
 
 public class Main {
--- Projeto/Pedido.java
+++ Projeto/Pedido.java
@@ -1,3 +1,9 @@
+/*
+Laura Rocha Yaguiu RA:10736399
+Janaína Passos RA: 10737888
+Bárbara Passos RA: 10737885
+*/
+
 public class Pedido {
   
   //construtuor Pedido()
