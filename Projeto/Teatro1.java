3b08f7c859d53b03c8fe3371204ccaaf3682087e
janaina-passos <janaina.diogopassos@gmail.com>
2025-11-01 10:41:22 -0300

    Create Teatro1.java

---
+++ Projeto/Teatro1.java
@@ -0,0 +1,27 @@
+public class Teatro {
+    
+  private Pedido carrinho; // = null
+  private Espetaculo espetaculoSelecionado; // = nulll
+
+  // construtuor Teatro()
+  public void novaCompra() {
+
+  }
+  
+  public void apresentaEspetaculos(){
+
+  }
+
+  public void selecionaEspetaculo(int numero){
+
+  }
+
+  public void novaEntrada(int tipo, int assento){
+
+  }
+
+  public double finalizaCompra(Strinf cpf){
+
+  }
+  
+}  
