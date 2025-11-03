e1aca35f9aa578acb52a36b1cf45b0f63985e6aa
babs162006 <barbara.diogopassos06@gmail.com>
2025-11-03 09:50:41 -0300

    Create Main2.java

---
+++ Projeto/Main2.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Teatro teatro = new Teatro();
+        int opcao = 0;
+
+        do {
+            System.out.println("\n*** MACK THEATHER ***");
+            System.out.println("(1) Cadastrar Espetáculo");
+            System.out.println("(2) Cadastrar Cliente");
+            System.out.println("(3) Compra de Entradas");
+            System.out.println("(4) Sair");
+            System.out.print("Selecione uma opção: ");
+            opcao = input.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    teatro.cadastrarEspetaculo();
+                    break;
+                case 2:
+                    teatro.cadastrarCliente();
+                    break;
+                case 3:
+                    teatro.compraDeEntradas();
+                    break;
+                case 4:
+                    System.out.println("Sistema encerrado");
+                    break;
+                default:
+                    System.out.println("Opção inválida! Tente novamente.");
+                    break;
+            }
+
+        } while (opcao != 4);
+
+        input.close();
+    }
+}
