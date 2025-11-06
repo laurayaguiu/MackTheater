533437b61454b12224dab407132899d9e9210651
janaina-passos <janaina.diogopassos@gmail.com>
2025-11-06 19:58:59 -0300

    Update Teatro1.java

--- Projeto/Teatro1.java
+++ Projeto/Teatro1.java
@@ -12,19 +12,28 @@ public class Teatro {
   //construtuor Teatro(){
 
   public void novaCompra() {
-
+    //cria novo pedido 
+    //chama o método apresentaEspetaculos()?
   }
   
   public void apresentaEspetaculos(){
-
+    //lista com os espetaculos 
   }
 
   public void selecionaEspetaculo(int numero){
-
+    //associar com o método apresentaAssentos() da classe Espetaculo?
   }
 
   public void novaEntrada(int tipo, int assento){
+    //associar com o método novaEntrada(int tipo, int assento) da classe Espetaculo?
+      
+    /* depois, na classe Espetaculo criar um new Entrada(this, assento) e associar com a classe Entrada?
+    chamar marcarAssento(assento) da classe Espetaculo
+    devolve a entrada para a classe Espetaculo
+    chamar adicionaEntrada(entrada) da classe Pedido
 
+    tudo associado? polimorfismo?
+    */
   }
 
   public double finalizaCompra(Strinf cpf){
