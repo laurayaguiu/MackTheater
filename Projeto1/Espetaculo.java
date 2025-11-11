76ba0776b7c4b7e96ecbf1a224bbc5cf0a08e96a
Laura Yaguiu <laurayaguiu@gmail.com>
2025-11-11 18:20:18 +0000

    class espetáculo comentada !

--- Projeto1/Espetaculo.java
+++ Projeto1/Espetaculo.java
@@ -1,3 +1,9 @@
+/*
+Laura Rocha Yaguiu RA:10736399
+Janaína Passos RA: 10737888
+Bárbara Passos RA: 10737885
+*/
+
 public class Espetaculo {
     private String nome;
     private String data;
@@ -5,41 +11,53 @@ public class Espetaculo {
     private double preco;
     private boolean[] assentos; // true = disponível, false = ocupado
 
+    //construtor inicializa
     public Espetaculo(String nome, String data, String hora, double preco) {
         this.nome = nome;
         this.data = data;
         this.hora = hora;
         this.preco = preco;
-        this.assentos = new boolean[50];
+        this.assentos = new boolean[50]; // já é definido, não é um dado q o cliente coloca
 
+         // inicializa todos os assentos como disponíveis (true = disponível) 
         for (int i = 0; i < assentos.length; i++) {
             assentos[i] = true;
         }
     }
 
+    //Mostrar assentos visualmente
     public void apresentaAssentos() {
         System.out.println("\n ||| Assentos Disponíveis |||");
-        int numero = 50;
+        int numero = 50; //quantidade de ass
+        //loop q percorre todos os ass
         for (int i = 0; i < assentos.length; i++) {
-            if (assentos[i]) {
+            if (assentos[i] == true) {
+                // numero formatado com 2 dígitos
                 System.out.printf("%02d ", numero);
             } else {
                 System.out.print("XX ");
             }
             numero--;
+            // a cada 10 pula linha - para mostrar certinho as fileiras
             if ((i + 1) % 10 == 0) System.out.println();
         }
     }
 
+    // nova entrada
     public Entrada novaEntrada(int tipo, int numero) {
+        // validar o número (null = ausência de objeto)
         if (numero < 1 || numero > 50) return null;
 
-        int indice = 50 - numero; // mapeia número do assento ao índice do array
+        // converter N do ass para indice do arry
+        // EX: o primeiro ass (50) é indice 0
+        int indice = 50 - numero; 
 
-        if (!assentos[indice]) {
-            return null; // já ocupado
+        // validar ass ocupado
+        if (assentos[indice] == false) {
+            return null; 
         }
 
+        // cria obj entrada, inicialmente sem valor
         Entrada entrada = null;
         switch (tipo) {
             case 1:
@@ -55,10 +73,12 @@ public class Espetaculo {
                 return null;
         }
 
-        assentos[indice] = false; // marca ocupado
+        // chamada de um método
+        marcarAssento(numero);// marca ocupado
         return entrada;
     }
 
+    //getters
     public double getPreco() {
         return preco;
     }
@@ -75,6 +95,11 @@ public class Espetaculo {
         return hora;
     }
 
+    /*
+   esse toString é sempre override pq vem da classe Object ("mãe de todas as class java")
+   todo objeto java herda o toString automaticamente
+   usamos como método de apoio - exibir informações no console
+   */ 
     @Override
     public String toString() {
         return "Espetáculo: " + nome +
@@ -83,6 +108,7 @@ public class Espetaculo {
                " | Preço da Inteira: R$ " + String.format("%.2f", preco);
     }
 
+    // método para marcar ass como ocupado
     public void marcarAssento(int numero) {
         if (numero < 1 || numero > 50) {
             System.out.println("Número de assento inválido!");
