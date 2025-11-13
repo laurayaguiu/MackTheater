86c274be6e93f61d0401c949777cb7c70791f0f6
babs162006 <barbara.diogopassos06@gmail.com>
2025-11-13 16:54:21 -0300

    Update Teatro.java

--- Projeto1/Teatro.java
+++ Projeto1/Teatro.java
@@ -9,10 +9,10 @@ import java.util.List;
 
 public class Teatro {
 
-    private Pedido carrinhoAtual; //pedido atual *
-    private Espetaculo espetaculoSelecionado; //Espetáculo da compra *
-    private List<Espetaculo> listaEspetaculos; //guarda todos os espetáculos 
-    private List<Cliente> clientes; //Guarda todos os clientes
+    private Pedido carrinhoAtual; 
+    private Espetaculo espetaculoSelecionado; 
+    private List<Espetaculo> listaEspetaculos;
+    private List<Cliente> clientes; 
 
     public Teatro() {
         this.listaEspetaculos = new ArrayList<>();
@@ -34,17 +34,14 @@ public class Teatro {
         return listaEspetaculos;
     }
 
-    // add novo espetaculo a lista
     public void adicionarEspetaculo(Espetaculo novoEspetaculo) {
         if (novoEspetaculo != null) listaEspetaculos.add(novoEspetaculo);
     }
 
-    // add novo cliente a lista
     public void adicionarCliente(Cliente novoCliente) {
         if (novoCliente != null) clientes.add(novoCliente);
     }
 
-    // cria pedido vazio ("canhirro de compras novo")*
     public void novaCompra() {
         this.carrinhoAtual = new Pedido();
     }
@@ -55,43 +52,33 @@ public class Teatro {
             System.out.println("Nenhum espetáculo cadastrado ainda.");
         } else {
             System.out.println("\n=== ESPETÁCULOS CADASTRADOS ===");
-            for (int i = 0; i < listaEspetaculos.size(); i++) {
-                Espetaculo e = listaEspetaculos.get(i);
-                System.out.println((i + 1) + " - " + e.toString());
+             for (int i = 0; i < listaEspetaculos.size(); i++) { //pega um espetáculo específico da lista 
+                Espetaculo e = listaEspetaculos.get(i); //guarda esse objeto na variável para poder trabalhar com ele dentro do laço
+                System.out.println((i + 1) + " - " + e.toString()); 
             }
         }
     }
-    //.size() devolve a quantidade de elementos , estilo .length mas agr com ArrayList
-    //get(i) acessa um objeto específico dentro da lista
-    //toString() da classe Espetaculo
 
-    //*
-    // quando cliente seleciona um espetáculo
     public boolean selecionaEspetaculo(int numero) {
-        //validacao
         if (numero < 1 || numero > listaEspetaculos.size()) {
             return false;
         }
 
-        // pega o valor (espetaculo) da posicao correta da lista e armazena em espetaculoSelecionado
+        ///guarda o espetáculo digitado em espetaculoSelecionado
         this.espetaculoSelecionado = listaEspetaculos.get(numero - 1);
         
-        // novo carrinho para espetáculo especificado
         novaCompra();
         return true;
     }
 
-    //*
-    // novo ingr ao carrinho atual
+  
     public boolean novaEntrada(int tipo, int assento) {
-        //validacao
         if (espetaculoSelecionado == null || carrinhoAtual == null) {
             return false;
         }
-        // cria um obj do tipo entrada q recebe uma nova entrada para o espetáculo selecionado
+        //Cria uma nova Entrada usando o método da classe Espetaculo
         Entrada entrada = espetaculoSelecionado.novaEntrada(tipo, assento);
        
-        //validacao
         if (entrada == null) {
             return false;
         }
@@ -100,9 +87,8 @@ public class Teatro {
         return true;
     }
 
-    //*
+    
     public double finalizaCompra(String cpf) {
-        // verifica se carrinho n esta vazio
         if (carrinhoAtual == null || carrinhoAtual.isVazio()) {
             return 0.0;
         }
@@ -121,24 +107,21 @@ public class Teatro {
         cliente.adicionaPedido(carrinhoAtual);
 
         
-        // calcula valor
+        
         double total = carrinhoAtual.calculaValorTotal();
 
-        // zera - limpa 
+        
         carrinhoAtual = null;
         espetaculoSelecionado = null;
 
         return total;
     }
 
-    //Procura o cliente dentro da lista clientes comparando o CPF
+   
     public Cliente encontraClientePorCpf(String cpf) {
         for (Cliente c : clientes) {
-            // verifica se o cpf em questao é igual ao que estou querendo e devolve esse cliente
             if (c.getCpf().equals(cpf)) return c;
         }
         return null;
     }
-    //c.getCpf() é cpf q estou passando
-    //.equals() usa para comparação, nao se usa ==
 }
