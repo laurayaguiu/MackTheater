df425ba7408a155d94fd0cf2636d7682219bf3e9
babs162006 <barbara.diogopassos06@gmail.com>
2025-11-13 16:40:14 -0300

    Update Entrada.java

--- Projeto1/Entrada.java
+++ Projeto1/Entrada.java
@@ -4,7 +4,7 @@ Janaína Passos RA: 10737888
 Bárbara Passos RA: 10737885
 */
 
-// class abs apenas para questoes de herança
+
 public abstract class Entrada {
     protected int numeroDoAssento;
     protected double precoBase;
